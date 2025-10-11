b0ceed92747d9cd52035006936ecb83a1e61be6b
Mudit Singh <muditsingh6624@gmail.com>
2025-10-12 02:21:28 +0530

    removed credentials :true

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,6 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
